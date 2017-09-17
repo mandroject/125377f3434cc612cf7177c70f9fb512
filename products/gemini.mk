741afdd071eac09a6a8c94266fbd53992ee63a48
Marulam Heryanto Samosir <marulam.samosir@gmail.com>
2017-09-18 01:18:06 +0700

    Add gemini product

---
+++ products/gemini.mk
@@ -0,0 +1,36 @@
+# Copyright (C) 2017 The Pure Nexus Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include pure telephony configuration
+include vendor/pure/configs/pure_phone.mk
+
+# Inherit AOSP device configuration for sailfish
+$(call inherit-product, device/google/marlin/gemini.mk)
+
+# Inherit arm64 phone gapps
+# $(call inherit-product-if-exists, vendor/gapps/gapps.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := gemini
+PRODUCT_DEVICE := gemini
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := MI 5
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="gemini" \
+    PRODUCT_NAME="gemini" \
+    BUILD_FINGERPRINT="Xiaomi/gemini/gemini:7.0/NRD90M/V8.2.1.0.NAACNEB:user/release-keys" \
+    PRIVATE_BUILD_DESC="gemini-user 7.0 NRD90M V8.2.1.0.NAACNEB release-keys"
